6d7f8e6acdc4391d7bef678356231064dbbe8ed3
szymonkluba <szymonkluba@gmail.com>
2022-01-08 01:32:56 +0100

    Changes in dropbox service, state for folder content.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   dropbox: {
-    apiKey: "uMHq35uknPEAAAAAAAAAAdpMjglMdLfjL4SHuEqm_16A_KCLvN2sH8pycwpQyAWI",
+    apiKey: process.env["DROPBOX_API_KEY"],
     listEntriesURL: "https://api.dropboxapi.com/2/files/list_folder",
     downloadFile: "https://content.dropboxapi.com/2/files/download",
     downloadZip: "https://content.dropboxapi.com/2/files/download_zip",
   },
   scraperApi: {
-    scrapRaceURL: "http://127.0.0.1:8000/scrap_race/",
+    scrapRaceURL: "https://fis-scraper-api.herokuapp.com/scrap_race/",
   }
 };
