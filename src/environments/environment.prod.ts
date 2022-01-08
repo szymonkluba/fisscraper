f584ecd70dae2ba080e783feb7d36d779b59bc54
szymonkluba <szymonkluba@gmail.com>
2022-01-08 01:39:34 +0100

    Heroku config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   dropbox: {
-    apiKey: process.env["DROPBOX_API_KEY"],
+    apiKey: "uMHq35uknPEAAAAAAAAAAdpMjglMdLfjL4SHuEqm_16A_KCLvN2sH8pycwpQyAWI",
     listEntriesURL: "https://api.dropboxapi.com/2/files/list_folder",
     downloadFile: "https://content.dropboxapi.com/2/files/download",
     downloadZip: "https://content.dropboxapi.com/2/files/download_zip",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   dropbox: {
-    apiKey: "test",
+    apiKey: "uMHq35uknPEAAAAAAAAAAdpMjglMdLfjL4SHuEqm_16A_KCLvN2sH8pycwpQyAWI",
     listEntriesURL: "https://api.dropboxapi.com/2/files/list_folder",
     downloadFile: "https://content.dropboxapi.com/2/files/download",
     downloadZip: "https://content.dropboxapi.com/2/files/download_zip",
