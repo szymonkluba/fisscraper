248400fd8e7c02e088335760aa0258b27fbe1b0f
szymonkluba <szymonkluba@gmail.com>
2022-01-10 15:56:21 +0100

    Adds notifications.

---
+++ src/app/state/notifications.actions.ts
@@ -0,0 +1,12 @@
+import { createAction, props } from "@ngrx/store";
+import { Notification } from "../models/notification.model";
+
+export const addNotification = createAction(
+  "[Notifications] add notification",
+  props<{ notification: Notification }>()
+)
+
+export const removeNotification = createAction(
+  "[Notifications] remove notification",
+  props<{ notification: Notification }>()
+)
---
+++ src/app/state/notifications.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from "@ngrx/store";
+import { addNotification, removeNotification } from "./notifications.actions";
+import { Notification } from "../models/notification.model";
+
+export const initialState: ReadonlyArray<Notification> = []
+
+export const notificationsReducer = createReducer(
+  initialState,
+  on(addNotification, (state, { notification }) => {
+    if (state.findIndex(element => element.id === notification.id) > -1) {
+      return state
+    }
+    return [...state, notification]
+  }),
+  on(removeNotification, (state, { notification }) => state.filter(element => element.id !== notification.id))
+)
