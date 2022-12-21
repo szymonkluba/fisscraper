c12c87ee0e772f4f23ee044fd9956431094fe228
szymonkluba <szymonkluba@gmail.com>
2022-12-21 20:45:57 +0100

    Fixed icons - finally:P

--- apps/fis-scraper-frontend/src/app/shared/interceptors/csrf_header/csrf-header.interceptor.ts
+++ apps/fis-scraper-frontend/src/app/shared/interceptors/csrf_header/csrf-header.interceptor.ts
@@ -11,12 +11,24 @@ import Cookies from 'js-cookie';
 
 @Injectable()
 export class CsrfHeaderInterceptor implements HttpInterceptor {
+  private readonly excludedUrls = ['.svg'];
+  private readonly excludedUrlsRegex: RegExp[] = this.excludedUrls.map(
+    urlPattern => new RegExp(urlPattern, 'i')
+  );
   constructor() {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    const passThrough: boolean = !!this.excludedUrlsRegex.find(regex =>
+      regex.test(request.url)
+    );
+
+    if (passThrough) {
+      return next.handle(request);
+    }
+
     request = request.clone({
       setHeaders: {
         'X-CSRFToken': Cookies.get('csrftoken'),
--- apps/fis-scraper-frontend/src/app/shared/interceptors/http-error/http-error.interceptor.ts
+++ apps/fis-scraper-frontend/src/app/shared/interceptors/http-error/http-error.interceptor.ts
@@ -10,12 +10,25 @@ import { NotificationsService } from '@services/notifications.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
+  private readonly excludedUrls = ['.svg'];
+  private readonly excludedUrlsRegex: RegExp[] = this.excludedUrls.map(
+    urlPattern => new RegExp(urlPattern, 'i')
+  );
+
   constructor(private readonly notificationService: NotificationsService) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    const passThrough: boolean = !!this.excludedUrlsRegex.find(regex =>
+      regex.test(request.url)
+    );
+
+    if (passThrough) {
+      return next.handle(request);
+    }
+
     return next.handle(request).pipe(
       catchError(err => {
         this.notificationService.handleErrorNotification(err);
